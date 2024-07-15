f2bcfc962d925926d572e90b52d36a90bf4c1e55
Dmitry Mashuk <dmitry.m@symfonyart.com>
2024-07-15 13:14:40 +0300

    Redis was added

--- app/app/Http/Controllers/CommentController.php
+++ app/app/Http/Controllers/CommentController.php
@@ -5,27 +5,43 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Services\Cache\RedisCacheService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
+    public function __construct(private RedisCacheService $redisCacheService) {
+    }
+
     public function index(): JsonResponse
     {
-        return response()->json(Comment::all());
+        $comments = $this->redisCacheService->get(config('redis_keys.comments'));
+
+        if ($comments) {
+            return response()->json(json_decode($comments, true));
+        }
+
+        $comments = Comment::all();
+
+        $this->redisCacheService->set(config('redis_keys.comments'), json_encode($comments));
+
+        return response()->json($comments);
     }
 
     public function store(Request $request): JsonResponse
     {
         $validated = $request->validate([
-            'post_id' => 'required|integer',
-            'user_id' => 'required|integer',
-            'content' => 'required|string',
-            'status' => 'integer',
+            'post_id'   => 'required|integer',
+            'user_id'   => 'required|integer',
+            'content'   => 'required|string',
+            'status'    => 'integer',
             'parent_id' => 'integer',
         ]);
 
         $comment = Comment::create($validated);
+
+        $this->redisCacheService->delete(config('redis_keys.comments'));
 
         return response()->json($comment, 201);
     }
@@ -38,14 +54,16 @@ class CommentController extends Controller
     public function update(Request $request, Comment $comment): JsonResponse
     {
         $validated = $request->validate([
-            'post_id' => 'integer',
-            'user_id' => 'integer',
-            'content' => 'required|string',
-            'status' => 'integer',
+            'post_id'   => 'integer',
+            'user_id'   => 'integer',
+            'content'   => 'required|string',
+            'status'    => 'integer',
             'parent_id' => 'integer',
         ]);
 
         $comment->update($validated);
+
+        $this->redisCacheService->delete(config('redis_keys.comments'));
 
         return response()->json($comment);
     }
@@ -54,6 +72,8 @@ class CommentController extends Controller
     {
         $comment->delete();
 
-        return response()->json(['message' => 'Success removed'], 204);
+        $this->redisCacheService->delete(config('redis_keys.comments'));
+
+        return response()->json(null, 204);
     }
 }
